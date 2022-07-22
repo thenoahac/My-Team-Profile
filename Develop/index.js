05678847e7fc17bc19acadf4f4453c410a6ec8a4
Noah Carralero <nole2kgames@gmail.com>
2022-07-22 04:05:28 -0400

    edited files

--- Develop/index.js
+++ Develop/index.js
@@ -6,30 +6,4 @@ const Manager = require('./lib/Manager')
 const inquirer = require('inquirer')
 const fs = require('fs');
 
-const addManager = ()=>{
-    return inquirer.prompt (
-        {
-            name: "name",
-            message: "What is the team manager's name?",
-            type: "input"
-        },
-        {
-            name: "id",
-            message: "What is the team manager's id?",
-            type: "input"
-        },
-        {
-            name: "name",
-            message: "What is the team manager's id?",
-            type: "input"
-        },
-        {
-            name: "name",
-            message: "What is the team manager's id?",
-            type: "input"
-        },
-    )
-}
-    then(answers => {
-    fs.writeFile(`./output/team-${answers.Manager.name}.html`, generateHTML, (err) => err ? console.error(err) : console.log('Member Added!'));
-})
+
